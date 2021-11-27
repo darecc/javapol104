c2163d12c074c6aa5a6c6a6744245806fe22e801
Radosław B <radek_b@op.pl>
2021-11-27 12:06:15 +0100

    najnowsza wersja zawierajaca dotychczasowe zadania : 1 i 2.

--- src/dc/pierwiastki.java
+++ src/dc/pierwiastki.java
@@ -4,31 +4,36 @@ import java.util.Scanner;
 
 public class pierwiastki {
     public static void main(String[] args) {
+        //- Pierwiastki równania kwadratowego y = ax2 + bx + c
         Scanner s = new Scanner(System.in);
         double a, b, c;
+        double d;
         double r1, r2, r3;
-        double delta;
 
-        System.out.println("Podaj a:");
+        System.out.println("Podaj a");
         a = s.nextDouble();
-
-        System.out.println("Podaj b:");
+        System.out.println("Podaj b");
         b = s.nextDouble();
-
-        System.out.println("Podaj c:");
+        System.out.println("Podaj c");
         c = s.nextDouble();
-        delta = Math.pow(b, 2) - (4*a*c);
 
-        if (a < 0) {
-            System.out.println("Brak pierwiastka");
+        d = (b * b) - 4 * a * c;
+
+        if (d < 0) {
+            System.out.println("Brak pierwiastków");
         }
-        else if (delta > 0) {
-            r1 = -b - Math.sqrt(delta) / (2 * a);
-            r2 = -b + Math.sqrt(delta) / (2 * a);
-            System.out.println("Wartość r1 wynosi: " + r1 + " a wartość r2 wynosi: " + r2);
-        } else {
+        else if (d > 0){
+
+            r1 = (-b - Math.sqrt(d)) / (2 * a);
+            r2 = (-b + Math.sqrt(d)) / (2 * a);
+
+            System.out.println("x1 wynosi : " + r1);
+            System.out.println("x2 wynosi : " + r2);
+
+        }
+        else {
             r3 = (-b) / (2 * a);
-            System.out.println("Wartość r3 wynosi:" + r3);
+            System.out.println("x0 wynosi : " + r3);
         }
 
 
