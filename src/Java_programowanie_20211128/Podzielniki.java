1aefd819a1c38f240ea371ae0f3fc72076bcea40
OlaRozak <ola.rozak@gmail.com>
2021-11-28 13:20:04 +0100

    Projekt z zad. 4 Podzielniki

---
+++ src/Java_programowanie_20211128/Podzielniki.java
@@ -0,0 +1,48 @@
+package Java_programowanie_20211128;
+
+import java.util.Scanner;
+
+/**
+ * Dla każdej liczby z przedziału od 1 do n znajdź wszystkie liczby o takiej samej sumie podzielników całkowitych.
+ */
+public class Podzielniki {
+    /**
+     * zdefiniowanie metody obliczającej sume podzielnikow
+      * @param liczba liczba, dla której obliczamy podzielniki
+     * @return suma podzielnikow
+     */
+    public static int SumaPodzielnikow(int liczba){
+        int suma=0;
+        for(int i =1; i<=(liczba/2);i++) {
+            if (liczba % i == 0) {
+                suma=suma+i;
+            }
+        }
+        return suma;
+    }
+
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Podaj rozmiar tablicy: ");
+        int n = S.nextInt();
+        if(n<3) {
+            System.out.println("Rozmiar tablicy musi być większy niz 2");
+            return;
+        }
+        int[] ciag = new int[n];
+
+        for (int i = 1; i < n-1; i++) {
+            ciag[i]=SumaPodzielnikow(i);
+        }
+
+    //porównanie
+        for (int i = 1; i < n-1; i++) {
+            for (int j = i +1; j < n; j++) {
+                if ( ciag[i] == ciag[j] ) {
+                    System.out.println(i + " >=< " + j);
+                }
+            }
+        }
+
+    }
+}
